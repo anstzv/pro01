dd1dfb936dff092f76447c7d25a9a6345901cf2e
anstzv <tzvetanovaanastasia@gmail.com>
2024-01-22 11:23:26 +0200

    test from gui

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@ using namespace std;
 int main(){
 
 
-cout<<"Hello GIT form home  work :) "<<endl;
+cout<<"Hello GIT form home  work :))) "<<endl;
 
  cout<<"Hello from dev :)";
+ cout<<"New line here"<<endl;
  return 0;
 }
 
